8d8c07175b4e7c89e177faabefc1ef679ea43245
shayleydon <shayleydon>
2025-08-22 14:21:45 +0100

    c12m2 auth END

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', TemplateView.as_view(template_name="index.html")),
     #path('logout/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),    
     path('djangoapp/', include('djangoapp.urls')),
